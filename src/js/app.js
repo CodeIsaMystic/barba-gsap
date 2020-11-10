6a5181863035e057448b72d62302e181aec3ee30
Julien Orjollet <oj.professionnel@gmail.com>
2020-11-10 11:49:28 +0100

    add animations & main tl on separates files

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,11 @@
 import barba from '@barba/core';
-import gsap from 'gsap';
+import gsap from 'gsap/gsap-core';
+import { animationEnter, animationLeave } from './animations';
 
-const animationEnter = (container) => {
-  return gsap.from(container, { autoAlpha: 0, duration: 2, clearProps: 'all', ease: 'none' });
-};
 
-const animationLeave = (container) => {
-  return gsap.to(container, { autoAlpha: 0, duration: 2, clearProps: 'all', ease: 'none' });
-};
+const resetActiveLink = () => gsap.set('a.is-active span', { xPercent: -100, transformOrigin: 'left' });
+
+
 
 barba.init({
   transitions: [
@@ -15,13 +13,18 @@ barba.init({
 
       /*  Barba  Hooks   */
       once({ next }) {
+        resetActiveLink();
+        gsap.from('header a', {
+          duration: 2,
+          yPercent: 100,
+          stagger: .2,
+          ease: 'power1.out',
+          onComplete: () => animationEnter(next.container)
+        })
 
-        /**  Page Loading  */
-        animationEnter(next.container);
       },
       leave: ({ current }) => animationLeave(current.container),
       enter({ next }) {
-        console.log('entering');
         animationEnter(next.container);
       }
     }
