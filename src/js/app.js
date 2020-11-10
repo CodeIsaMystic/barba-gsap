c20832ccabd9c78a64313ffead6c2107cc10b09e
Julien Orjollet <oj.professionnel@gmail.com>
2020-11-10 16:34:36 +0100

    review of prefetch, views, router..

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
 import barba from '@barba/core';
+import barbaRouter from '@barba/router';
+import barbaPrefetch from '@barba/prefetch';
 import gsap from 'gsap/gsap-core';
 import {
   animationEnter,
@@ -9,11 +11,39 @@ import {
 } from './animations';
 
 
+
+const myRoutes = [
+  { name: 'home', path: '/index.html' },
+  { name: 'architecture', path: '/architecture.html' },
+  { name: 'detail', path: '/detail-page.html' },
+  { name: 'detail-2', path: '/detail-page-2.html' }
+]
+
+
+barba.use(barbaRouter, {
+  routes: myRoutes
+});
+//barba.use(barbaPrefetch);
+
+
 const resetActiveLink = () => gsap.set('a.is-active span', { xPercent: -100, transformOrigin: 'left' });
 
 
+barba.hooks.enter((data) => {
+  console.log({ data });
+  window.scrollTo(0, 0);
+});
+
 
 barba.init({
+  view: [
+    {
+      namespace: 'architecture',
+      beforeEnter(data) {
+        console.log(data)
+      }
+    }
+  ],
   transitions: [
     {
       name: 'detail',
@@ -42,6 +72,21 @@ barba.init({
       },
       leave: ({ current }) => animationLeave(current.container),
       enter({ next }) {
+        animationEnter(next.container);
+      }
+    }, {
+      name: 'from-detail',
+      from: {
+        namespace: ['detail']
+      },
+      leave: ({ current }) => leaveFromProject(current.container),
+      enter({ next }) {
+        gsap.from('header a', {
+          duration: .6,
+          yPercent: 100,
+          stagger: .2,
+          ease: 'power1.out'
+        });
         animationEnter(next.container);
       }
     }
